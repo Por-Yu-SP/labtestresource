ea9bb1fa763972bd0d1475e72ec64de2e5d553f6
PorYu <yu07.24@ichat.sp.edu.sg>
2025-11-24 22:51:09 +0800

    Implement PyTest test cases

--- test_labsample1.py
+++ test_labsample1.py
@@ -3,11 +3,20 @@ import labsample1
 
 def test_get_longest_workout():
     workouts = labsample1.load_csv()
+    result=labsample1.get_longest_workout(workouts)
+    expected=75.0
+    assert(expected==result["duration"])
 
 
 def test_calc_total_duration():
     workouts = labsample1.load_csv()
+    result=labsample1.calc_total_duration(workouts)
+    expected=853
+    assert(expected==result)
 
 
 def test_calc_average_duration():
     workouts = labsample1.load_csv()
+    result=labsample1.calc_average_duration(workouts)
+    expected=42.65
+    assert(expected==result)
